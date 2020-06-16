29c5eff2c84a6b2fb7dc3cf4b7a7be451193aa69
Vaibhav Bhatia <vaibhavbhatia604@gmail.com>
2020-06-16 15:54:29 +0530

    new

---
+++ js/web.js
@@ -0,0 +1,187 @@
+
+//js for shrinking nav
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ) {
+      // document.getElementById("top-u").style.fontSize="300%";
+      // document.getElementById("top-u").className = "cl1a";
+      // document.getElementById("top-u").className = "cl2a";
+      
+//      document.getElementById("logo").className = "effect";
+      // document.getElementById("top-b").style.fontSize = "0.95rem";
+      // document.getElementById("logo").style.height = "64px";
+  } else {
+      // document.getElementById("top-u").style.fontSize="350%";
+      // document.getElementById("top-u").className = "cl1";
+      // document.getElementById("top-u").className = "cl2";
+//      document.getElementById("logo").className = "test";
+      // document.getElementById("top-b").style.fontSize = "1rem";
+    // document.getElementById("logo").style.height = "110px";
+  }
+}
+
+//-----------------------------------------------------------------------------
+//
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction(),scrollFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("top-b");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+      // slider.style.paddingTop="58px";
+  } else {
+    navbar.classList.remove("sticky");
+      // slider.style.paddingTop="0px";
+  }
+}
+ //-----------------------------------------------------------------------------------
+// Wrap every letter in a span
+$('.ml14 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml14 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeInOutExpo",
+    duration: 900
+  }).add({
+    targets: '.ml14 .letter',
+    opacity: [0,1],
+    translateX: [40,0],
+    translateZ: 0,
+    scaleX: [0.3, 1],
+    easing: "easeOutExpo",
+    duration: 800,
+    offset: '-=600',
+    delay: function(el, i) {
+      return 150 + 25 * i;
+    }
+  }).add({
+    targets: '.ml14',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  //-------------------------------------------------------------------------------------------------
+ 
+//-----------------------------------------------------------------------------------------------------
+
+//---------------------gallery w3
+// Open the Modal
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+// ------------------------------------------------------------tabs
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+
+// Wrap every letter in a span
+$('.ml10 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: function(el, i) {
+      return 45 * i;
+    }
+  }).add({
+    targets: '.ml10',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
+  // top button
+
+  // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop >800) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
